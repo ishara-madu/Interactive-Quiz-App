c37f4caa88829b8278bcf2bffa35a13d917ba14d
Ishara <isharamadushankab@gmail.com>
2024-12-15 15:55:04 +0530

    modified QuestionActivity.kt

---
+++ app/src/main/java/com/pixeleye/myapplication/Constant.kt
@@ -0,0 +1,81 @@
+package com.pixeleye.myapplication
+
+object Constant{
+    fun getQuestion():ArrayList<QuestionClass>{
+        val questionList = ArrayList<QuestionClass>()
+
+        val qu1 = QuestionClass(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.angola,
+            "Angola",
+            "Afghanistan",
+            "Albania",
+            "Andorra",
+            1
+        )
+        questionList.add(qu1)
+
+        val qu2 = QuestionClass(
+            2,
+            "What country does this flag belong to?",
+            R.drawable.turkey,
+            "Angola",
+            "Afghanistan",
+            "Turkey",
+            "Andorra",
+            3
+        )
+        questionList.add(qu2)
+
+        val qu3 = QuestionClass(
+            3,
+            "What country does this flag belong to?",
+            R.drawable.australia,
+            "Angola",
+            "Afghanistan",
+            "Australia",
+            "Andorra",
+            3
+        )
+        questionList.add(qu3)
+
+        val qu4 = QuestionClass(
+            4,
+            "What country does this flag belong to?",
+            R.drawable.azerbaijan,
+            "Angola",
+            "Azerbaijan",
+            "Australia",
+            "Andorra",
+            2
+        )
+        questionList.add(qu4)
+
+        val qu5 = QuestionClass(
+            5,
+            "What country does this flag belong to?",
+            R.drawable.costa_rica,
+            "Argentina",
+            "Afghanistan",
+            "Australia",
+            "Costa Rica",
+            4
+        )
+        questionList.add(qu5)
+
+        val qu6 = QuestionClass(
+            6,
+            "What country does this flag belong to?",
+            R.drawable.united_states,
+            "Belgium",
+            "USA",
+            "Australia",
+            "Andorra",
+            2
+        )
+        questionList.add(qu6)
+
+        return questionList
+    }
+}
--- app/src/main/java/com/pixeleye/myapplication/QuestionActivity.kt
+++ app/src/main/java/com/pixeleye/myapplication/QuestionActivity.kt
@@ -1,6 +1,10 @@
 package com.pixeleye.myapplication
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,30 @@ class QuestionActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val progressText = findViewById<TextView>(R.id.prgressStatue)
+        val title = findViewById<TextView>(R.id.question)
+        val flag = findViewById<ImageView>(R.id.flagIcon)
+        val option1 = findViewById<TextView>(R.id.firstAnswer)
+        val option2 = findViewById<TextView>(R.id.secondAnswer)
+        val option3 = findViewById<TextView>(R.id.thirdAnswer)
+        val option4 = findViewById<TextView>(R.id.fourthAnswer)
+
+
+        val questionList = Constant.getQuestion()
+        val currentPosition = 0
+        val question = questionList[currentPosition]
+        progressBar.progress = currentPosition
+        progressText.text = "${currentPosition}/${questionList.size}"
+        title.text = question.question
+        flag.setImageResource(question.image)
+        option1.text = question.option1
+        option2.text = question.option2
+        option3.text = question.option3
+        option4.text = question.option4
+
+
+
+
     }
 }
