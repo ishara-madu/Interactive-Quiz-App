a2b81e03aa7282105570e508836c41e4c1feddaf
Ishara <isharamadushankab@gmail.com>
2024-12-15 12:54:44 +0530

    first commmit

---
+++ app/src/main/java/com/pixeleye/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.pixeleye.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn : Button = findViewById(R.id.startButton)
+        val name : EditText = findViewById(R.id.name)
+
+        btn.setOnClickListener{
+            if(name.text.toString().isEmpty()){
+                Toast.makeText(this,"Please enter your name",Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this,QuestionActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
